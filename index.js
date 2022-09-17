f841ec85260cfeb65dff7cc862beeee6623c6f15
Asamu victor <asamuvictor03@gmail.com>
2022-09-17 09:33:56 +0100

    connected with mongodb

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose")
 require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT
+const URI= process.env.URI
+mongoose.connect(URI,(err)=>{
+    if(err){
+        console.log("there is an error in connecting")
+    }else{
+        console.log("mongoose has connected successfully")
+    }
+})
 app.use(express.urlencoded({extended:true}));
 const signupRouter = require("../vihat backend/routes/Signup.route");
 app.use("/auth",signupRouter);
