d147d88d25e9d6efcfb1d367bb0cc61f04e10807
Asamu victor <asamuvictor03@gmail.com>
2022-09-17 21:44:50 +0100

    completed signup controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,5 @@
+const signUpModel = require("../models/Signup.model")
+
 const signup = (req,res)=>{
     res.send({you:[
         {name:"asamu victor",age:19,company:"andela.com"},
@@ -7,6 +9,15 @@ const signup = (req,res)=>{
 
 const signuppost=(req,res)=>{
     console.log(req.body)
-    res.send({message:"sucess"})
+    let form = signUpModel(req.body);
+    form.save((err)=>{
+        if(err){
+            console.log(err)
+            res.send({message:"signup not succesful",status:false})
+        }else{
+            console.log("success")
+            res.send({message:"signup successful",status:true})
+        }
+    })
 }
 module.exports={signup,signuppost};
--- models/Signup.model.js
+++ models/Signup.model.js
@@ -1,9 +1,23 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const signupSchema= mongoose.Schema({
-    firsname:{required:true,type:String},
+    firstname:{required:true,type:String},
     lastname:{required:true,type:String},
     email:{required:true,type:String,unique:true},
     password:{required:true,type:String}
 });
 
-const signUpModel= mongoose.model("signup_collection",signupSchema)
+let saltRound = 10;
+signupSchema.pre("save",function(next){
+    console.log(this.password)
+    bcrypt.hash(this.password,saltRound,(err,hashedPassword)=>{
+        if(!err){
+            console.log(hashedPassword)
+            this.password=hashedPassword
+            next()
+        }
+    })
+})
+
+const signUpModel= mongoose.model("signup_collection",signupSchema);
+module.exports=signUpModel;
